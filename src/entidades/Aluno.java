7b0e272e33029b2c5de50d7422748c2da4401ac9
Renan <renan.giev@gmail.com>
2025-07-06 23:54:24 -0300

    atualizeidiagrama

--- src/entidades/Aluno.java
+++ src/entidades/Aluno.java
@@ -6,7 +6,8 @@ public class Aluno extends Usuario implements Serializable {
     private String curso;
     private int semestre;
 
-    public Aluno(String nome, String email, String telefone, String senha, String matricula, String curso, int semestre) {
+    public Aluno(String nome, String email, String telefone, String senha, String matricula, String curso,
+            int semestre) {
         super(nome, email, telefone, senha, matricula);
         this.curso = curso;
         this.semestre = semestre;
--- src/entidades/Professor.java
+++ src/entidades/Professor.java
@@ -7,7 +7,7 @@ public class Professor extends Servidor implements Serializable {
     private String cargoAcademico;
 
     public Professor(String nome, String email, String telefone, String senha, String matricula,
-                     String cursoMinistrado, String cargoAcademico) {
+            String cursoMinistrado, String cargoAcademico) {
         super(nome, email, telefone, senha, matricula);
         this.cursoMinistrado = cursoMinistrado;
         this.cargoAcademico = cargoAcademico;
